161b2caf53b8a991d2839a39a7cc0376153ebd3e
dependable-helper-6910 <ivana_pile@yahoo.com>
2019-09-09 03:15:56 +0000

    Done.

--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -1,3 +1,20 @@
+
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
+
 require 'spec_helper'
 
 describe '#reverse_each_word' do
